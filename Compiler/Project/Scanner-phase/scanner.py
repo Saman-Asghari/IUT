8981a0d8d486ab5c43a8d0ae31861355f5b54dbb
Saman-Asghari <samansadat1383@gmail.com>
2025-05-09 19:44:20 +0330

    updated the scanner and added some text file

--- Compiler/Project/Scanner-phase/scanner.py
+++ Compiler/Project/Scanner-phase/scanner.py
@@ -1,17 +1,18 @@
 from utils import KEYWORDS ,  TokenType , SYMBOLS
 
+#the class that scanns everything
 class Scanner:
   #initializing the first state of scanner
   def __init__(self,input_file):
-    self.input_file=input_file
-    self.line_number=0
-    self.current_pos=0
-    self.current_line=''
-    self.tokens=[]
-    self.errors=[]
-    self.symbol_table={kew : kew for kew in sorted(KEYWORDS)}
-    self.input_lines=[]
-    self.in_comment=False
+    self.input_file=input_file   #we save file here
+    self.line_number=0    
+    self.current_pos=0          #cursor
+    self.current_line=''    # we save each line to process
+    self.tokens=[]      #we save tokens here
+    self.errors=[]      #we save errors here
+    self.symbol_table={kew : kew for kew in sorted(KEYWORDS)}     #we load symbol table here
+    self.input_lines=[]       #we save lines here
+    self.in_comment=False       #some variable for comment handeling
     self.comment_start_line=0
     self.comment_content_start=0
   
@@ -21,7 +22,7 @@ class Scanner:
     try:
       with open(self.input_file,'r') as f:
         self.input_lines=f.readlines()
-      
+      #we prcess each line and add the tokens and the errors here
       for line in self.input_lines:
         self.line_number +=1
         self.current_line=line
@@ -31,6 +32,7 @@ class Scanner:
       self.check_for_unclosed_comment()
       return self.tokens , self.errors , self.symbol_table
 
+    #if we couldn't read the file
     except IOError:
       self.errors.append((0,f"unable to open the file : {self.input_file}"))
       return self.tokens , self.errors , self.symbol_table
@@ -38,9 +40,9 @@ class Scanner:
 
   def process_line(self):
     #processing each line and realizing the errors and the tokens
-    while self.current_pos < len(self.current_line):
+    while self.current_pos < len(self.current_line): #in this while loop we start from the begining of the line and go unitl end of it
       char = self.current_line[self.current_pos]
-
+      #bunch of if statment to see what should it do for each char it read
       if self.skip_whitespace(char):
         continue
 
@@ -63,6 +65,7 @@ class Scanner:
         self.process_invalid_char(char)
 
 
+  #this function handels the things to be ignored
   def skip_whitespace(self,char):
     if char in {' ','\t','\v','\f','\r'}:
       self.current_pos += 1
@@ -94,6 +97,8 @@ class Scanner:
       return True
 
     return False
+  
+  #we process if the number is in a correct format or not here
   def process_number(self):
     start_pos = self.current_pos
     while self.has_more_chars() and self.current_line[self.current_pos].isdigit():
@@ -109,6 +114,7 @@ class Scanner:
     self.add_token(TokenType.NUM, num_str)
 
 
+  #we process the identifiers here
   def process_identifier(self):
     start_pos=self.current_pos
     while (self.has_more_chars() and (self.current_line[self.current_pos].isalnum() or self.current_line[self.current_pos]=='_')):
@@ -130,20 +136,23 @@ class Scanner:
         self.symbol_table[id_str] = id_str
   
 
+
+  #in here we process the symbols 
   def process_symbol(self):
     char=self.current_line[self.current_pos]
     next_char=self.peek_next_char()
-    if char == '=' and next_char =='#':
+    if char == '=' and next_char =='#':  #invalid cause we dont have =#
       self.errors.append((self.line_number, '(=#, Invalid input)'))
-      self.current_pos+=2
+      self.current_pos+=2 #we go to the 2 next char
       return
 
-    if char == '/' and next_char == '/':
+    if char == '/' and next_char == '/': #invalid cause we dont have /
       self.errors.append((self.line_number, '(/, Invalid input)'))
       self.errors.append((self.line_number, '(/, Invalid input)'))
       self.current_pos += 2
       return
     
+    #we process the correct symbols here
     two_char = char + next_char if next_char else char
     if two_char in {'+=', '*=', '/=', '-=', '<=', '==', '!=', '/*', '*/'}:
       if two_char in SYMBOLS:
@@ -155,24 +164,35 @@ class Scanner:
       self.add_token(TokenType.SYMBOL, char)
       self.current_pos += 1
   
+
+
+  #in this function we detect the invalid output
   def process_invalid_char(self,char):
     self.errors.append((self.line_number, f'({char}, Invalid input)'))
     self.current_pos += 1
     self.skip_invalid_input(False)
   
+  #in this function we detect the invalid output
   def skip_invalid_input(self,is_invalid_number):
     if is_invalid_number:
-      self.current_pos += 1
-            
+      self.current_pos += 1 # we ignore this number and we want to skip this number so we should reach the end of the number
+
+
+    #in this while loop we go until we reach the end of it        
     while self.has_more_chars():
       char = self.current_line[self.current_pos]
       if char.isspace() or char in SYMBOLS or char.isalnum():
         break
       self.current_pos += 1
   
+
+  #in this function we add the token we found to the token list
   def add_token(self, token_type, token_value):
     self.tokens.append((self.line_number, (token_type, token_value)))
   
+
+
+#this functin checks if the comment is closed or not(if not it saves in errors list)
   def check_for_unclosed_comment(self):
     if self.in_comment:
       comment_content = self.input_lines[self.comment_start_line - 1][self.comment_content_start:]
@@ -181,11 +201,15 @@ class Scanner:
         preview += "..."
       self.errors.append((self.comment_start_line, f'(/*{preview}, Unclosed comment)'))
     
+
+#this function we only see the next char for token and error handeling
   def peek_next_char(self):
     if self.current_pos + 1 < len(self.current_line):
-      return self.current_line[self.current_pos + 1]
+      return self.current_line[self.current_pos + 1]  #it returns the next char
     return None
   
+
+  #this function checks if the word we want to process cross the end of line or not
   def has_more_chars(self):
     return self.current_pos < len(self.current_line)
 
